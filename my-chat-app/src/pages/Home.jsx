3d01a4b29e13a8aeb9cb86320944ff368fd6cd25
rata <rataprim30026@gmail.com>
2023-04-30 10:52:15 +0700

    update frontend page

---
+++ my-chat-app/src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import {Link} from 'react-router-dom'
+import {FaRocketchat} from 'react-icons/fa'
+import { useSelector } from 'react-redux'
+
+
+function Home(){
+    const {user} = useSelector((state)=>state.auth);
+    return (
+        <>
+            <section className='heading'>
+                <h1>Server-Client Chat System</h1>
+                <p>An easy way to start chatting</p>
+            </section>
+            <div>
+            {user?(
+                    <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+                    <FaRocketchat/>Start Chatting
+                    </Link>
+                ):(
+                    <>
+                    </>
+                )}
+            </div>
+            {/* <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+            <FaQuestionCircle/>Create New Appointment
+            </Link>
+
+            <Link to='/tickets' className='btn btn-block'>
+            <FaTicketAlt/>View My Appointments
+            </Link> */}
+        </>
+    )
+}
+
+export default Home;
