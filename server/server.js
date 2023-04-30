4e179e3b9ed3c1bdd5ed1bcb411d8b3d45e1f797
Witsaroot <pccphet77@gmail.com>
2023-04-30 16:52:32 +0700

    Change port to 4000

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-//const socketio = require('socket.io');
+const http = require('http');
+const socketio = require('socket.io');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -42,8 +43,13 @@ app.use('/api/auth', authRoutes);
 // });
 
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
+const PORT = process.env.PORT || 4000;
+const server = http.createServer(app);
+// Add this
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+server.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
@@ -51,18 +57,19 @@ process.on('unhandledRejection', (err, promise) => {
     //Close server & exit process
     server.close(() => process.exit(1));
 });
-        // const io = require("socket.io")(server, {
-        //     pingTimeout: 60000,
-        //     cors: {
-        //     //origin: "http://localhost:3000",
-        //     //origin : "https://sec33-group3-muse-connect-5kdwszn2t-pacharawin.vercel.app"
-        //     origin : '*'
-        //     },
-        // });
-        // io.on('connection', (socket) => {
-        //     console.log(`Socket connected: ${socket.id}`);
+
+// const io = require("socket.io")(server, {
+//     pingTimeout: 60000,
+//         cors: {
+//         origin: "http://localhost:3000",
+//         origin : "https://sec33-group3-muse-connect-5kdwszn2t-pacharawin.vercel.app"
+//             origin : '*'
+//             },
+//         });
+//         io.on('connection', (socket) => {
+//             console.log(`Socket connected: ${socket.id}`);
             
-        //     socket.on('disconnect', () => {
-        //         console.log(`Socket disconnected: ${socket.id}`);
-        //     });
-        // });
+//             socket.on('disconnect', () => {
+//                 console.log(`Socket disconnected: ${socket.id}`);
+//             });
+//         });
